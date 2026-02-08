f991021eb1b8bc861f3927c510ca8326a9eba179
tsogoo <tsogoorbt98@gmail.com>
2026-02-08 20:56:24 +1100

    error handling

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ def main():
     random_number = random.randint(1, 100)
     #  Set the number of guesses allowed
     guess_count = 7
+    # win flag
+    won = False
     #  Start the game loop
     # guess = int(input("Guess the number between 1 and 100: "))
     while guess_count > 0:
-        guess = int(input("Guess the number between 1 and 100: "))
+        try:
+            guess = int(input("Guess the number between 1 and 100: "))
+        except ValueError:
+            print("Please enter a valid integer.")
+            continue
         if guess == random_number:
+            won = True
             print("Congratulations! You guessed the number! Number: ", random_number)
             break
         else:
@@ -21,13 +28,19 @@ def main():
         print("You have", guess_count, "guesses left.")
 
     # Check if the user guessed the number correctly
-    if guess != random_number:
+    if not won:
         # Inform the user that they ran out of guesses
         print("You ran out of guesses. The number was:", random_number)
+# main()
+# try_again = input("Do you want to play again? (y/n): ")
+# if try_again.lower() == "y":
+#     main()
+
 # Run the game
-main()
-# Ask the user if they want to play again
-try_again = input("Do you want to play again? (y/n): ")
-# If the user wants to play again, call the main function again
-if try_again.lower() == "y":
+while True:
     main()
+    # Ask the user if they want to play again
+    try_again = input("Do you want to play again? (y/n): ")
+    # If the user wants to play again, call the main function again
+    if try_again.lower() != "y":
+        break
